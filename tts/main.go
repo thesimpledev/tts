22f6c1c708bbf3ef3808e49028bb2095be044eff
Steven Stanton <steven@thesimpledev.com>
2024-06-23 10:01:59 -0400

    Fixed typo in read me

--- tts/main.go
+++ tts/main.go
@@ -284,7 +284,7 @@ func printHelp() {
 	-m model defaults to tts-1-hd
 		Model options are: tts-1 and tts-1-hd
 
-	-b laces buffer words at start and end of text to help with abrupt 
+	-b places buffer words at start and end of text to help with abrupt 
 		starts and ends
 
 
